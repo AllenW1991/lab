abf8c28505b5fa0c90527fc71e511ebd5cdff5a6
AllenW1991 <venecia631201@yahoo.com.tw>
2021-10-06 16:32:53 +0800

    more

--- ajax/index.js
+++ ajax/index.js
@@ -1,25 +1,31 @@
 $("#validation_form").submit(function secure(event) {
+    let error_message = "";
     event.preventDefault();
+
     let name = $("#e-mail-name").val();
     let email_address = $("#e-mail-address").val();
     let pass1 = $("#pass1").val();
     let pass2 = $("#pass2").val();
     let message = $("#e-mail-message").val();
 
-
     function is_valid(e_mail) {
         let pattern = /^\b[A-Za-z0-9]+@[A-Za-z0-9]+\.com\b/i;
         return pattern.test(email_address);
     }
 
-
     if (name == "" || email_address == "" || pass1 == "" || pass2 == "") {
-        let error_message = "請輸入完整信息";
-        alert(error_message);
+        error_message = "請輸入完整信息";
+
     } else if (is_valid(email_address) == false) {
-        alert("請輸入正確格式信箱");
+        error_message = "請輸入正確格式信箱";
     } else if (pass1 != pass2) {
-        alert("")
+        error_message = "兩次密碼輸入不符";
     }
 
+    if (error_message == "") {
+        $("#error").css("color", "rgb(124, 230, 138)")
+        $("#error").html("訊息已傳送");
+    } else {
+        $("#error").html(`${error_message}`);
+    }
 });
